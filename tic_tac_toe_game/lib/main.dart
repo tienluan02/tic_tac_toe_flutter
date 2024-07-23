ce305a361dff0adfa2f6942e97170362f4e2ae9c
tienluandev <nguyentienluan3@gmail.com>
2024-07-23 11:38:24 +0700

    creating settings page

--- tic_tac_toe_game/lib/main.dart
+++ tic_tac_toe_game/lib/main.dart
@@ -144,6 +144,27 @@ class ReferencesPage extends StatelessWidget {
   }
 }
 
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('Audio'),
+          ]
+        )
+      )
+    );
+  }
+}
+
+
 class MyHomePage extends StatefulWidget {
   final String player1;
   final String player2;
